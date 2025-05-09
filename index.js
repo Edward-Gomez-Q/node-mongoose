e6ef17ce05c2e0c9bded7dc7c2ad8694e58e8e66
Edward-Gomez-Q <osval_edward_13@hotmail.com>
2025-05-08 22:45:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // Definir las rutas
 app.use('/api/clientes', require('./routes/clientes'));
 app.use('/api/pedidos', require('./routes/pedidos'));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
